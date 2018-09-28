dc23eafab04bfa0f5ebc6d1bf7cd9d00b03f7c2a
runtu <ky3466214728@gmail.com>
2018-09-28 21:15:43 +0800

    update

---
+++ app/settings.py
@@ -0,0 +1,21 @@
+"""
+  Created by kebo on 2018/7/28
+"""
+import os
+from app import app
+
+SQLALCHEMY_DATABASE_URI = r'sqlite:///service.sqlite3'
+
+SECRET_KEY = 'whxylaf'
+
+UPLOAD_FOLDER = os.path.join(app.static_folder, 'uploads')
+ALLOWED_EXTENSIONS = {'jpg', 'png', 'gif'}
+
+ADVICE_PATH = UPLOAD_FOLDER + os.path.sep + 'advice.txt'
+
+# 最大 1M
+MAX_CONTENT_LENGTH = 1 * 1024 * 1024
+
+SERVER = 'https://ikebo.cn/service'
+
+EMAIL_PWD = 'yang4ever'
